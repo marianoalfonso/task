059bda66a4835025712c0dc804378de5cb08ce68
Mariano.Alfonso <Mariano.Alfonso@RTMX-ITM-42>
2023-03-18 11:06:34 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMain = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTask1 = new System.Windows.Forms.TextBox();
+            this.txtTask2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFlow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtMain
+            // 
+            this.txtMain.Location = new System.Drawing.Point(31, 137);
+            this.txtMain.Multiline = true;
+            this.txtMain.Name = "txtMain";
+            this.txtMain.Size = new System.Drawing.Size(733, 36);
+            this.txtMain.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start task";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTask1
+            // 
+            this.txtTask1.Location = new System.Drawing.Point(31, 302);
+            this.txtTask1.Multiline = true;
+            this.txtTask1.Name = "txtTask1";
+            this.txtTask1.Size = new System.Drawing.Size(733, 36);
+            this.txtTask1.TabIndex = 2;
+            // 
+            // txtTask2
+            // 
+            this.txtTask2.Location = new System.Drawing.Point(34, 380);
+            this.txtTask2.Multiline = true;
+            this.txtTask2.Name = "txtTask2";
+            this.txtTask2.Size = new System.Drawing.Size(733, 36);
+            this.txtTask2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "task 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "task 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "main program";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "flow";
+            // 
+            // txtFlow
+            // 
+            this.txtFlow.Location = new System.Drawing.Point(34, 223);
+            this.txtFlow.Multiline = true;
+            this.txtFlow.Name = "txtFlow";
+            this.txtFlow.Size = new System.Drawing.Size(733, 36);
+            this.txtFlow.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 487);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFlow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTask2);
+            this.Controls.Add(this.txtTask1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMain;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTask1;
+        private System.Windows.Forms.TextBox txtTask2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFlow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            txtMain.Text = string.Empty;
+            txtFlow.Text = string.Empty; 
+            txtTask1.Text = string.Empty; 
+            txtTask2.Text = string.Empty;  
+
+            var task1 = new Task(()=>
+            {
+                Thread.Sleep(5000);
+                txtTask1.Text = "finalizacion task1 (5 segundos)";
+            });
+            task1.Start();
+
+
+            var task2 = new Task(() =>
+            {
+                Thread.Sleep(3000);
+                txtTask2.Text = "finalizacion task2 (3 segundos)";
+            });
+            task2.Start();
+
+            txtFlow.Text = "realizando tareas";
+
+            await task1;
+            await task2;
+
+            txtMain.Text = "fin tarea general";
+
+        }
+
+
+    }
+}
